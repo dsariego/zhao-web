7103c4ac4bc5ad7f86009dbdf4c2f37565a18305
dsariego <dsariego@uoc.com>
2021-12-06 15:49:11 +0100

    Primera subida

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,6 +3,9 @@
  * see commented examples below
  */
 
+import * as bootstrap from 'bootstrap';
+
+import '@fortawesome/fontawesome-free/js/all.js';
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 
@@ -10,7 +13,47 @@
  * Write any other JavaScript below
  */
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+ const gallery = document.querySelector('.gallery');
+ const overlay = document.querySelector('.overlay');
+ const overlayImage = overlay.querySelector('img');
+ const overlayClose = overlay.querySelector('.close');
+
+ function generateHTML([h, v]) {
+   return `
+   <div class="photo h${h} v${v}">
+     <img src="https://picsum.photos/id/${randomNumber(60)}/800/400/">
+     <div class="item__overlay">
+       <button class="btn__lite">View </button>
+     </div>
+   </div>`;
+ }
+
+ function randomNumber(limit) {
+   return Math.floor(Math.random() * limit) + 1;
+ }
+
+ function handleClick(e) {
+   console.log(e.currentTarget);
+   const src = e.currentTarget.querySelector('img').src;
+   overlayImage.src = src;
+   overlay.classList.add('open');
+ }
+
+ function close() {
+   overlay.classList.remove('open');
+ }
+
+ const digits = Array.from({length: 50}, () =>
+ [randomNumber(4), randomNumber(4)]).concat([[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]]);
+ console.log(digits);
+
+ const html = digits.map(generateHTML).join('');
+ console.log(html);
+
+ gallery.innerHTML = html;
+
+ const items = document.querySelectorAll('.photo');
+
+ items.forEach(item => item.addEventListener('click',handleClick));
+
+ overlayClose.addEventListener('click', close);
